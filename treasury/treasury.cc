1c4966239371aa3f413a90584445722523eae2a7
LaoZhang <samuelzhang77@yahoo.com>
2020-02-29 18:42:29 +1000

    Fix bug in treasury

--- treasury/treasury.cc
+++ treasury/treasury.cc
@@ -9,6 +9,7 @@ int find_stair(int **r, int s, int m)
     int i, p;
 
     p = r[s][1];
+	i = s;
 
     while (p) {
         if (r[i][0]) {
@@ -25,33 +26,38 @@ int main()
 {
     int n, m, s, i, cnt;
     int ***a;
-    string input, tmp;
-    stringstream ss;
 
-    getline(cin, input);
-    ss << input;
-
-    ss >> n >> m;
+    cin >> n >> m;
 
     a = new int **[n];
     for (i = 0; i < n; i++) {
         a[i] = new int *[m];
+    }
+    for (i = 0; i < n; i++) {
         for (int j = 0; j < m; j++)
             a[i][j] = new int[2];
     }
 
     for (i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            getline(cin, input);
-            ss << input;
-            ss >> a[i][j][0] >> a[i][j][1];
+            cin >> a[i][j][0] >> a[i][j][1];
         }
     }
     cin >> s;
+#if 0
+	for (i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << a[i][j][0] << ' ' << a[i][j][1] << '\t';
+        }
+		cout << endl;
+    }
+#endif
 
     for (int st = s, i = 0; i < n; i++) {
-        st = find_stair(a[i], st, m);
-        cnt += a[i][st][1];
+		cnt += a[i][st][1];
+		//cout << a[i][st][1] << ' ' << cnt << endl;
+        //cout << '#' << i << "st = " << st << endl;
+		st = find_stair(a[i], st, m);
     }
     cout << cnt << endl;
 
