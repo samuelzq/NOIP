aa4733b31b373e0001494957cff67f45b11ac198
LaoZhang <samuelzhang77@yahoo.com>
2020-03-06 13:38:30 +1000

    Add jinming

--- jinming/jinming.cc
+++ jinming/jinming.cc
@@ -9,19 +9,21 @@ int main()
     int **f, *w, *v;
 
     cin >> t >> m;
-    w = new int[m]();
-    v = new int[m]();
+    w = new int[m + 1]();
+    v = new int[m + 1]();
 	f = new int *[m + 1]();
 	for (i = 0; i < m + 1; i++)
 		f[i] = new int[t + 1]();
 
-    for (i = 0; i < m; i++)
+    for (i = 1; i < m + 1; i++) {
         cin >> w[i] >> v[i];
+		v[i] *= w[i];
+	}
 
     for(i = 1; i <= m; i++) {
         for(j = 1; j <= t; j++) {
-            if (j >= w[i - 1])
-                f[i][j] = MAX(f[i - 1][j - w[i - 1]] + v[i], f[i - 1][j]);
+            if (j >= w[i])
+                f[i][j] = MAX(f[i - 1][j - w[i]] + v[i], f[i - 1][j]);
 			else
 				f[i][j] = f[i - 1][j];
         }
