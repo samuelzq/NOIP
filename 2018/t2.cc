64f604ab0047fbebc5da357827bb64104110e30a
LaoZhang <samuelzhang77@yahoo.com>
2020-12-12 20:41:04 +1000

    Modify T2 to ACed code

--- 2018/t2.cc
+++ 2018/t2.cc
@@ -1,51 +1,52 @@
 #include <iostream>
-
+#include <cmath>
+#include <cstdio>
 using namespace std;
-
+#define LL long long
 int main()
 {
-	int n, i;
-	long long m, p1, s1, s2, p2, dragon, tiger, diff;
-	long long *soldier, *power;
+    int n, i, m, p1, p2;
+    LL s1, s2;
+    cin >> n;
+    LL c[n + 1];
+    for(i = 1; i <= n; i++)
+    {
+        cin >> c[i];
+    }
+    cin >> m >> p1 >> s1 >> s2;
+    c[p1] += s1;
+    LL lPower = 0; // 龙势力
+    for(i = 1; i < m; i++)
+    {
+        lPower += c[i] * (m - i);
+    }
+    LL rPower = 0;
+    for(i = m + 1; i <= n; i++)
+    {
+        rPower += c[i] * (i - m);
+    }
 
-	cin >> n;
-	soldier = new long long[n]();
-	power = new long long[n]();
-
-	for (i = 0; i < n; i++)
-		cin >> soldier[i];
-	cin >> m >> p1 >> s1 >> s2;
-
-	m--;
-	for (i = 0, dragon = 0; i < m; i++)
-		dragon += soldier[i] * (m - i);
-	for (i = m + 1, tiger = 0; i < n; i++)
-		tiger += soldier[i] * (i - m);
-	p1--;
-	if (p1 < m)
-		dragon += s1 * (m - p1);
-	if (p1 > m)
-		tiger += s1 * (p1 - m);
-
-	if (dragon > tiger) {
-		long tmp = 0;
-		tmp = diff = dragon - tiger;
-		for (p2 = m + 1; p2 < n; p2++) {
-			if (diff <= ((p2 - m) * s2))
-				break;
-		}
-	} else if (dragon < tiger) {
-		long tmp = 0;
-		tmp = diff = tiger - dragon;
-		for (p2 = m - 1; p2 >= 0; p2--) {
-			if (diff <= (m - p2) * s2)
-				break;
-		}
-		if (p2 < 0)
-			p2 = 0;
-	} else {
-		p2 = m;
-	}
-	cout << p2 + 1 << endl;
-	return 0;
+    LL gap = abs(lPower - rPower); // 双方的气势差
+    p2 = m;          // 暂放在m的位置上
+    float dist = gap * 1.0 / s2; // s2离m兵营的距离
+    if(rPower > lPower)  // 虎势力大，s2要放在龙方
+    {
+        //这里floor()也可以改为int()
+        p2 = m - floor(dist + 0.5);
+        if(p2 < 1)
+        {
+            p2 = 1; // 不能越界
+        }
+    }
+    else if(rPower < lPower)
+    {
+        // 注意，这里0.5是要舍弃掉的，大于0.5才加1
+        p2 = m + ceil(dist - 0.5);
+        if(p2 > n)
+        {
+            p2 = n; // 不能越界
+        }
+    }
+    cout << p2 << endl;
+    return 0;
 }
